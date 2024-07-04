81e1132e2d1decc63ad83422b35815a79f90ce84
Aram Avakyan <avakanaram20@gmail.com>
2024-07-04 16:39:50 +0400

    react_init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,26 @@
-import { createElement } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export const App = () => {
-	const currentYear = new Date().getFullYear(); // императивный стиль
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement(
-				'p',
-				null,
-				'Edit ',
-				createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('p', null, currentYear),
-		),
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
 	);
 };
+
+export default App;
